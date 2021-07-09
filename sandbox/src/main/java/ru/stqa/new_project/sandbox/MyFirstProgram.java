2b0dd190f2007b9d20f08eca15a6208495ec9abc
jackTret <83885201+jackTret@users.noreply.github.com>
2021-07-09 19:51:41 +0300

    Написал код согласно лекции 1.4

--- sandbox/src/main/java/ru/stqa/new_project/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/new_project/sandbox/MyFirstProgram.java
@@ -3,7 +3,11 @@ package ru.stqa.new_project.sandbox;
 public class MyFirstProgram {
 
  public static void main(String[] args) {
-	System.out.println("Hello, New_world! I'm robot");
+	System.out.println("Hello, New_world! " + "I'm robot");
+
+	int l = 8;
+	int s = l * l;
+	System.out.println("Площадь квадрата со стороной " + l + " = " + s);
  }
  
 }
