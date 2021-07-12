b6147183df8a45014ab2f8c41072b4ccae4e691b
jackTret <83885201+jackTret@users.noreply.github.com>
2021-07-12 20:20:21 +0300

    Внес изменения в настройки gradle

---
+++ sandbox/src/main/java/ru/stqa/new_project/sandbox/Square.java
@@ -0,0 +1,13 @@
+package ru.stqa.new_project.sandbox;
+
+public class Square {
+
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+  public double area() {
+    return this.l * this.l;
+  }
+}
