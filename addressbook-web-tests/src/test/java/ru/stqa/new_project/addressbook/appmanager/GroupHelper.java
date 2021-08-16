f57c3848c1f2e9ed7bb387bf68546c74e5886b45
jackTret <83885201+jackTret@users.noreply.github.com>
2021-08-16 23:01:25 +0300

    Сократил количество кода, ввел повсеместное использование уникальных идентификаторов

--- addressbook-web-tests/src/test/java/ru/stqa/new_project/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/new_project/addressbook/appmanager/GroupHelper.java
@@ -5,9 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import ru.stqa.new_project.addressbook.model.GroupData;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class GroupHelper extends BaseHelper {
 
@@ -27,8 +25,8 @@ public class GroupHelper extends BaseHelper {
     click(By.name("delete"));
   }
 
-  public void selectGroup(int index) {
-    wd.findElements(By.name("selected[]")).get(index).click();
+  public void selectGroupById(int id) {
+    wd.findElement(By.cssSelector("input[value='" + id + "']")).click();
     //click(By.name("selected[]"));
   }
 
@@ -45,28 +43,26 @@ public class GroupHelper extends BaseHelper {
   }
 
   public void create(GroupData group) {
-    //returnToGroupPage();
     initGroupCreation();
     fillGroupForm(group);
     submitGroupCreation();
     returnToGroupPage();
-    //returnToHomePage();
   }
-  public void modify(int index, GroupData group) {
-    selectGroup(index);
+  public void modify(GroupData group) {
+    selectGroupById(group.getId());
     initGroupModification();
     fillGroupForm(group);
     submitGroupModification();
     returnToGroupPage();
   }
-  public void delete(int index) {
-    selectGroup(index);
+
+  public void delete(GroupData group) {
+    selectGroupById(group.getId());
     deleteSelectedGroups();
     returnToGroupPage();
   }
 
   public void returnToHomePage() {
-    //click(By.xpath("//a[contains(@href, './')]"));
     click(By.linkText("home"));
     //click(By.xpath("//div[@id='content']/div/i/a[2]"));
   }
@@ -79,8 +75,8 @@ public class GroupHelper extends BaseHelper {
     return wd.findElements(By.name("selected[]")).size();
   }
 
-  public List<GroupData> list() {
-    List<GroupData> groups = new ArrayList<GroupData>();
+  public Set<GroupData> all() {
+    Set<GroupData> groups = new HashSet<GroupData>();
     List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
     for (WebElement element : elements){
       String name = element.getText();
