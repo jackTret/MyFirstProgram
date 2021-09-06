67ae5f8f0a8fe5455c4a020abbe7a8a1aeac9577
jackTret <83885201+jackTret@users.noreply.github.com>
2021-09-07 01:02:52 +0300

    Реализовал провайдеры тестовых данных, которые читают данные из файла Json

--- addressbook-web-tests/src/test/java/ru/stqa/new_project/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/new_project/addressbook/tests/ContactCreationTest.java
@@ -1,12 +1,21 @@
 package ru.stqa.new_project.addressbook.tests;
 
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.new_project.addressbook.model.ContactData;
 import ru.stqa.new_project.addressbook.model.Contacts;
 import ru.stqa.new_project.addressbook.model.GroupData;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -21,12 +30,26 @@ public class ContactCreationTest extends TestBase{
     }
   }
 
-  @Test (enabled = true)
-  public void testContactCreationTests() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validContactsFromJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null) {
+      json += line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType()); //List<ContactData>.class
+    return contacts.stream().map((c) -> new Object[] {c}).collect(Collectors.toList()).iterator();
+  }
+
+  @Test(dataProvider = "validContactsFromJson")
+  public void testContactCreationTests(ContactData contact) throws Exception {
     app.contact().ContactHomePage();
     Contacts before = app.contact().all();
-    File photo = new File("src/test/resources/kitten_child.png");
-    ContactData contact = new ContactData()
+    //File photo = new File("src/test/resources/kitten_child.png");
+    /*ContactData contact = new ContactData()
             .withName("Vladimir")
             .withMidName("Ivanovich")
             .withLastname("Zgardanov")
@@ -40,21 +63,13 @@ public class ContactCreationTest extends TestBase{
             .withE_mail("Zgardanych777@gmail.com")
             .withE_mailNew("Zgardanych787@gmail.com")
             .withE_mailWork("Zgardanych797@gmail.com")
-            .withGroup("test1");
+            .withGroup("test1");*/
     app.contact().create(contact);
+    assertThat(app.contact().count(), equalTo(before.size() + 1));
     Contacts after = app.contact().all();
-    assertThat(after.size(), equalTo(before.size() + 1));
-    /*assertThat(after, equalTo(
-            before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));*/
+    //assertThat(app.contact().count(), equalTo(before.size() + 1));
+    assertThat(after, equalTo(
+            before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
   }
-
-  /*@Test (enabled = true)
-  public void testCurrentDir() {
-    File currentDir = new File(".");
-    System.out.println(currentDir.getAbsolutePath());
-    File photo = new File("src/test/resources/kitten_child.png");
-    System.out.println(photo.getAbsolutePath());
-    System.out.println(photo.exists());
-  }*/
 }
 
